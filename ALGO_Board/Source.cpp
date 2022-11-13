b2c4623dd42a06ce65c6822702603507a24794ec
Danielsio <kingddd301@gmail.com>
2022-11-13 23:49:33 +0200

    need to test

--- ALGO_Board/Source.cpp
+++ ALGO_Board/Source.cpp
@@ -1,55 +1,66 @@
 #include <fstream>
 #include <array>
 #include <vector>
-
+#include <queue>
+#include <iostream>
 using namespace std;
+
+#define MAXCOLS 1000
+#define MAXROWS 1000
 
 typedef struct Point 
 {
 	int x;
 	int y;
+
+	Point(int x = 0, int y = 0) :x(x), y(y) {} // defult ctor
 } Point;
+
+typedef struct Node {
+	Point p;
+	int distance;
+} Node;
 
 int N = 0;
 int M = 0;
 
-
+//--------------------------------------------------------------------------------------//
+bool isValid(int row, int col);
+void writeAnswerToFile(string fileName, bool foundPath, string path);
+void solve(vector<vector <bool>>& boardAsGraph, vector<Point>& ghosts, Point& player, vector<Point>& destinations);
+void BFS(vector<vector <bool>>& mat, Point src, Point dest, string& path);
+void readFile(string fileName, Point& player, vector<Point>& ghosts, vector<Point>& destinations, vector<vector <char>>& board);
+//--------------------------------------------------------------------------------------//
 
 int main(int argc, char* argv[])
 {
-
 	vector<Point> ghosts;
 	Point player;
 	vector<Point> destinations;
-
-	char** board = readScreenFile(argv[1] );
+	vector<vector <char>> board;
+	readFile(argv[1], player, ghosts, destinations, board);
 
 	vector<vector <bool>> boardAsGraph(N);
-
-
-
-	// converts screen array to bool array
 	for (int i = 0; i < N; i++)
-		for (int j = 0; j < M; j++)
-		{
-			boardAsGraph[i].push_back(false);
-		}
-
-	for (int i = 0; i < N; i++)
-	{
 		for (int j = 0; j < M; j++)
 		{
 			if (board[i][j] != '#')
 			{
-				boardAsGraph[i][j] = true;
+				boardAsGraph[i].push_back(true);
+			}
+			else 
+			{
+				boardAsGraph[i].push_back(false);
 			}
 		}
-	}
-
-
-
+	solve(boardAsGraph, ghosts, player, destinations);
 }
-
+//--------------------------------------------------------------------------------------//
+bool isValid(int row, int col)
+{ 
+	return (row >= 0) && (col >= 0) && (row < MAXROWS) && (col < MAXCOLS); 
+}
+//--------------------------------------------------------------------------------------//
 void solve(vector<vector <bool>>& boardAsGraph, vector<Point>& ghosts, Point& player, vector<Point>& destinations)
 {
 	string currentPath;
@@ -74,39 +85,33 @@ void solve(vector<vector <bool>>& boardAsGraph, vector<Point>& ghosts, Point& pl
 		BFS(boardAsGraph, player, destinations[i], currentPath);
 		if (currentPath.length() < minPathSteps)
 		{
-			// func return path + YES
+			writeAnswerToFile("res", true, currentPath);
 			return;
 		}
-
-
 	}
 
-	// func return NO
+	writeAnswerToFile("res", false, "");
 }
-
-
-
+//--------------------------------------------------------------------------------------//
 void BFS(vector<vector <bool>>& mat,Point src, Point dest, string& path)
 {
-	string path = "";
-
-		int dRow[] = { -1, 0, 0, 1 };
+	int dRow[] = { -1, 0, 0, 1 };
 	int dCol[] = { 0, -1, 1, 0 };
 
 	// Stores the distance for each
 	// cell from the source cell
-	int d[N][M];
+	int d[1000][1000];
 	memset(d, -1, sizeof d);
 
 	// Distance of start cell is 0
-	d[src.GetY()][src.GetX()] = 0;
+	d[src.y][src.x] = 0;
 
 	// Initialize a visited array to track the positions we are going to explore
-	bool visited[N][M];
+	bool visited[1000][1000];
 	memset(visited, false, sizeof visited);
 
 	// Mark source cell as visited
-	visited[src.GetY()][src.GetX()] = true;
+	visited[src.y][src.x] = true;
 
 	// Create a queue for BFS algoritem
 	queue<Node> q;
@@ -122,17 +127,17 @@ void BFS(vector<vector <bool>>& mat,Point src, Point dest, string& path)
 
 		// Deque front of the queue
 		Node curr = q.front();
-		Point p = curr.pt;
+		Point p = curr.p;
 
 		// If the destination cell is reached, then find the path
-		if (p.GetX() == dest.GetX() && p.GetY() == dest.GetY())
+		if (p.x == dest.x && p.y == dest.y)
 		{
-			int x = p.GetX(), y = p.GetY();
-			int dist = curr.dist;
+			int x = p.x, y = p.y;
+			int dist = curr.distance;
 			// Assign the distance of destination to the distance matrix
-			d[p.GetY()][p.GetX()] = dist;
+			d[p.y][p.x] = dist;
 			// Iterate until source is reached
-			while (x != src.GetX() || y != src.GetY())
+			while (x != src.x || y != src.y)
 			{
 
 				// Append DOWN
@@ -174,8 +179,8 @@ void BFS(vector<vector <bool>>& mat,Point src, Point dest, string& path)
 		// Explore all adjacent directions
 		for (int i = 0; i < 4; i++)
 		{
-			int row = p.GetY() + dRow[i];
-			int col = p.GetX() + dCol[i];
+			int row = p.y + dRow[i];
+			int col = p.x + dCol[i];
 
 			// If the current cell is valid
 			// cell and can be traversed
@@ -185,186 +190,72 @@ void BFS(vector<vector <bool>>& mat,Point src, Point dest, string& path)
 				visited[row][col] = true;
 
 				// Enqueue the adjacent cells
-				Node adjCell = { { col, row }, curr.dist + 1 };
+				Node adjCell = { { col, row }, curr.distance + 1 };
 				q.push(adjCell);
 
 				// Update the distance
 				// of the adjacent cells
-				d[row][col] = curr.dist + 1;
+				d[row][col] = curr.distance + 1;
 			}
 		}
 	}
 	// If the destination is not reachable
 	if (!missionAcomplished)
 		return;
-
-
-
-
-
-	return path;
 }
-
-
-
-
-char** readScreenFile(string fileName) // opened the next screen file, read the data from it, and writes it to the 'board'
+//--------------------------------------------------------------------------------------//
+void readFile(string fileName, Point& player, vector<Point>& ghosts, vector<Point>& destinations, vector<vector <char>>& board) // opened the next screen file, read the data from it, and writes it to the 'board'
 {
 	int i, j;
-	int ghostCount = 0;
-	int countBread = 0;
 	char currChar;
-	bool isLegendInLine;
-	bool isHavePacman = false;
-	bool isHaveLegend = false;
-	char prevChar;
-	int placeOfLegend;
 
-	ifstream screenFile(fileName);
+	ifstream file(fileName);
 
-	if (!screenFile.is_open()) // fails to open
+	if (!file.is_open()) // fails to open
 	{
-		isOpened = false;
-		//throw screenName[curScreen];
+		cout << "cant open file" << endl;
 	}
 
-	for (i = 0; i < MAX_ROWS; i++) // fills the board, and converts the file's data to the borad's symbols
-	{
-		j = 0;
-		isLegendInLine = false;
-		currChar = screenFile.get();
+	// read N & M first
+	file >> N >> M;
 
-		while (j < MAX_COLS)
+	for (i = 0; i < N; i++) // fills the board, and converts the file's data to the borad's symbols
+		for (j = 0; j < M; j++)
 		{
-			prevChar = currChar;
+			currChar = file.get();
 
 			switch (currChar)
 			{
-			case (char)File_Symbols::PACMAN_FILE:
-				pacman.setStartPos(Coord(j, i));
-				currChar = (char)Board_Symbols::SPACE;
-				isHavePacman = true;
+			case 'A':
+				player = Point(j, i);
 				break;
 
-			case (char)File_Symbols::GHOST_FILE:
-				if (ghostCount < MAX_GHOSTS)
-				{
-					(*ghosts[ghostCount]).setStartPos(Coord(j, i));
-					ghostCount++;
-				}
-				currChar = (char)Board_Symbols::SPACE;
+			case 'M':
+				ghosts.push_back(Point(j, i));
 				break;
 
-			case (char)File_Symbols::LEGEND_FILE:
-				board.setLegendPos(Coord(j, i));
-				currChar = (char)Board_Symbols::SPACE;
-				placeOfLegend = j;
-				isLegendInLine = true;
-				isHaveLegend = true;
-				break;
-
-			case (char)File_Symbols::WALL_FILE:
-				currChar = (char)Board_Symbols::WALL;
-				break;
-
-			case (char)Board_Symbols::SPACE:
-				if (j >= board.getWidth())
-				{
-					currChar = (char)File_Symbols::LIMITS;
-				}
-				else
-				{
-					currChar = (char)Board_Symbols::BREAD;
-					countBread++;
-				}
-				break;
-
-			case (char)File_Symbols::LIMITS://%
+			case '.': // exit points == dest
+				destinations.push_back(Point(j, i));
 				break;
 
 			default:
-				currChar = (char)File_Symbols::LIMITS;
+				cout << "bad file. could not read all characters";
+				return;
 			}
-
-			board.setCell(i, j, currChar);
-			j++;
-
-			currChar = screenFile.get();
-
-			if (currChar == END_LINE)
-			{
-				if (i == 0)
-				{
-					if (prevChar == (char)File_Symbols::LEGEND_FILE)
-					{
-						if (j + 19 > MAX_COLS)
-						{
-							board.setWidth(MAX_COLS);
-						}
-						else
-						{
-							board.setWidth(j + 19);
-						}
-					}
-					else
-					{
-						board.setWidth(j);
-					}
-				}
-
-				j = MAX_COLS;
-			}
-
-			if (screenFile.eof())
-			{
-				if (isLegendInLine)
-				{
-					if (i + 2 >= MAX_ROWS)
-					{
-						i = MAX_ROWS - 1;
-					}
-					else
-					{
-						i += 2;
-					}
-				}
-
-				board.setHight(i + 1);
-				j = MAX_COLS;
-				i = MAX_ROWS;
-			}
+			board[i].push_back(currChar);
 		}
-
-		while (currChar != END_LINE && !screenFile.eof())
-		{
-			currChar = screenFile.get();
-		}
-	}
-
-	if (pacman.getCurPos().getX() >= board.getWidth()) // checks the file vaildation
-	{
-		isHavePacman = false;
-	}
-	if (board.getLegendPos().getX() >= board.getWidth())
-	{
-		isHaveLegend = false;
-	}
-
-	if (isHavePacman && isHaveLegend)
-	{
-		isOpened = true;
-
-		board.setCountBread(countBread);
-
-		board.fixLegendArea();
-
-		numOfGhosts = ghostCount;
-	}
-	else
-	{
-		isOpened = false;
-		throw screenName[curScreen];
-	}
-
 }
 //--------------------------------------------------------------------------------------//
+void writeAnswerToFile(string fileName, bool foundPath, string path)
+{
+	ofstream file(fileName);
+	if (foundPath) 
+	{
+		file << "YES" << endl;
+		file << path;
+	}
+	else 
+	{
+		file << "NO" << endl;
+	}
+}
